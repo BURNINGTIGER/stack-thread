c96b6adc914c0370c486bcab08217459c25caeba
BURNINGTIGER <Aleks203467@gmail.com>
2017-10-30 12:52:24 +0300

    Update stack.cpp

--- sources/stack.cpp
+++ sources/stack.cpp
@@ -1,1 +1,44 @@
 #include "stack.hpp"
+#include <string>
+#include <chrono>
+#include <thread>
+
+void producer(stack<int> &Stack)
+{
+	for (;;)
+	{
+		Stack.push(std::rand() % 100);
+		std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (3) + 1));
+	}
+}
+
+void consumer(stack<int> &Stack)
+{
+	for (;;)
+	{
+		try
+		{
+			Stack.pop();
+		}
+		catch (std::logic_error)
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (4) + 1));
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(std::rand() % (4) + 1));
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	stack<int> Stack;
+
+	std::thread pr(producer, std::ref(Stack));
+	std::thread cn(consumer, std::ref(Stack));
+
+	pr.join();
+	cn.join();
+
+
+	return 0;
+}
